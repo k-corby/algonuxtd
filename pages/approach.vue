476e7e380567589f6fc15961139d6d7de36194d1
Kane Corby <kcorby@Kanes-MacBook-Pro.local>
2021-05-23 21:19:49 +1000

    approach

---
+++ pages/approach.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="bg-red container-fluid" style="overflow:hidden;">
+      <div class="header circle-pattern">
+        <div id="banner" class="">
+          <h1 class="br-top text-offwhite" id="title">
+            <span style="font-size:26vw;">Our Approach.</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid" style="padding-top: 18vw;">
+      <div class="row pl pr">
+        <div class="col-12">
+          <h2 class="poppins" style="line-height:85%;">
+            Simple<br />
+            done right.
+          </h2>
+          <hr />
+        </div>
+      </div>
+      <div class="row pl pr pb">
+        <div class="col-7">
+          <p>
+            Objectives inform students the learning outcomes of the class. What
+            will they know? What will they be able to do? Why is this important
+            to know? It's an effective way to assess their learning progress.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <img
+          class="scale-crop"
+          src="https://images.pexels.com/photos/273230/pexels-photo-273230.jpeg?cs=srgb&dl=pexels-pixabay-273230.jpg&fm=jpg"
+        />
+      </div>
+    </div>
+
+    <div class="container-fluid bg-mustard" style="padding-top: 18vw;">
+      <div class="row pl pr">
+        <div class="col-12">
+          <h2 class="text-white libre" style="font-size:17vw;">
+            user centred
+          </h2>
+          <hr />
+        </div>
+      </div>
+      <div class="row pl pr">
+        <div class="col-7">
+          <p>
+            We implement user-centered design (UCD) that focus on putting users
+            at the center of product design and development.
+          </p>
+        </div>
+      </div>
+      <div class="row pl pr pb">
+        <div class="col-12">
+          <Accordion />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+hr {
+  background-color: black;
+  margin: 4vw 0;
+  height: 0.5vw;
+  width: 10vw;
+}
+h2 {
+  font-size: 13vw;
+  font-weight: 800;
+  line-height: 75%;
+  letter-spacing: -0.8vw;
+  color: #222;
+}
+
+p {
+  margin: 0 0 5vw 0;
+  font-size: 180%;
+  line-height: 40px;
+}
+</style>
